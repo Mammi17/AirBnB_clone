3d437f0fa9cd65bdd651b1cb1ae5598b5331ed9e
hadjara <hadjaramamane61@gmail.com>
2023-07-16 18:08:24 -0700

    task 0

---
+++ pycodestyle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Write beautiful code that passes the pycodestyle checks"""
+
+class DemClass:
+    """that shows how to document a class"""
+
+    def demo_method(self):
+        """This is a demo method that shows how to document a method"""
+        pass
